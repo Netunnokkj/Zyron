381fab034cae7b8209769d829c103b14422ebb23
yLuckDev <gabrielhenriquelinssxw@gmail.com>
2022-09-07 16:23:11 -0300

    up

--- src/Commands/Categories/Util.js
+++ src/Commands/Categories/Util.js
@@ -73,7 +73,7 @@ export default {
       }
     } else if (interaction.options.getSubcommand() === 'serverinfo') {
       try {
-        const GuildID = interaction.guild || client.guilds.fetch(`${interaction.options.getString('guild')}`);
+        const GuildID = client.guilds.fetch(`${interaction.options.getString('guild')}`) || interaction.guild;
 
        // const EmbedSP = new EmbedBuilder().setColor(client.color.default).setAuthor({ name: ``, iconURL: interaction.guild.iconURL() })
 
