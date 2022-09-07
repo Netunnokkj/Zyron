cde7bba6390bf98039c90cee92b1a32b7d3bac32
yLuckDev <gabrielhenriquelinssxw@gmail.com>
2022-09-07 16:21:23 -0300

    up

--- src/Structures/Client.js
+++ src/Structures/Client.js
@@ -75,8 +75,10 @@ export default class ZyronClient extends Client {
           if (!pull.name) return;
           this.commands.set(pull.name, pull);
           
-          await rest.put(Routes.applicationCommands("694901042986614805"), { body: slashsArray });
-        
+          this.on('ready', async () => {
+            await rest.put(Routes.applicationCommands("694901042986614805"), { body: slashsArray });
+          })
+                  
          };
         });
       } catch (err) {
