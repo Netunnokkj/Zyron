eeec44d00753aec8a54184d189f66effd1dae764
yLuckDev <gabrielhenriquelinssxw@gmail.com>
2022-09-07 17:13:35 -0300

    up

--- src/Structures/Client.js
+++ src/Structures/Client.js
@@ -75,7 +75,7 @@ export default class ZyronClient extends Client {
           if (!pull.name) return;
           this.commands.set(pull.name, pull);
           
-            this.application.commands.set(pull.name, pull)
+            this.application.commands.set(slashsArray)
             // await rest.put(Routes.applicationCommands("694901042986614805"), { body: slashsArray });
           
                   
